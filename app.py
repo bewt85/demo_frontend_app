4b6b7babea46dc88b0beea7ac2f2f50726b301cb
Ben Taylor <ben@drie.co>
2016-04-14 11:33:42 +0100

    Basic app, works on my laptop

---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+
+from flask import Flask, request
+from jinja2 import Template
+
+app = Flask(__name__)
+
+template = Template("""\
+<h1>Hello {{ name }}</h1>
+<p>Welcome to a basic demo</p>
+""")
+
+@app.route("/")
+def hello():
+    name = request.args.get("name", "World")
+    return template.render(name=name)
+
+if __name__ == "__main__":
+    kwargs = {}
+    if os.getenv("DEBUG"):
+        kwargs["debug"] = True
+    if os.getenv("PORT"):
+        kwargs["port"] = os.getenv("PORT")
+    app.run(**kwargs)
